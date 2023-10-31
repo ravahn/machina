9adefb29e57d3c2cdec078a3106732119c9811ff
Ravahn <Ravahn@hotmail.com>
2023-10-31 14:00:44 -0400

    v2.3.8.8 - updates for Global 6.51 & Korean 6.40 patches

--- Machina.FFXIV/Headers/Opcodes/Server_MessageType.cs
+++ Machina.FFXIV/Headers/Opcodes/Server_MessageType.cs
@@ -1,4 +1,4 @@
-﻿// Copyright © 2021 Ravahn - All Rights Reserved
+﻿// Copyright © 2023 Ravahn - All Rights Reserved
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
