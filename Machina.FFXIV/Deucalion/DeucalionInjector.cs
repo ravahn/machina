3d95df75679f006f5e6382319fa7eab195e164b3
Ravahn <Ravahn@hotmail.com>
2024-07-16 13:19:25 -0400

    v2.4.0.1 - Update Deucalion to 1.1.0, update opcodes for 7.01

--- Machina.FFXIV/Deucalion/DeucalionInjector.cs
+++ Machina.FFXIV/Deucalion/DeucalionInjector.cs
@@ -122,7 +122,7 @@ namespace Machina.FFXIV.Deucalion
 
         #endregion
 
-        private static readonly string _resourceFileName = "deucalion-1.0.0.dll";
+        private static readonly string _resourceFileName = "deucalion-1.1.0.dll";
 
         public static string LastInjectionError { get; internal set; }
 
@@ -158,8 +158,10 @@ namespace Machina.FFXIV.Deucalion
                 }
             }
 
-            string release_checksum = "7c-58-2e-e4-7c-74-1e-a8-49-54-4f-72-4c-1f-af-0e-46-09-e0-39-74-c7-97-94-ac-37-42-dd-10-2d-f4-07"; // 1.0.0
-
+            //string release_checksum = "7c-58-2e-e4-7c-74-1e-a8-49-54-4f-72-4c-1f-af-0e-46-09-e0-39-74-c7-97-94-ac-37-42-dd-10-2d-f4-07"; // 1.0.0
+            string release_checksum = "19-94-1f-2b-ff-b9-d5-1f-92-cd-60-10-5d-25-cd-19-0c-65-78-0f-6c-a0-70-8b-d8-48-3a-4a-fd-df-ea-93"; // 1.1.0
+            
+            //
             // validate checksum
             byte[] checksum = CalculateChecksum(fileName);
             if (!string.Equals(BitConverter.ToString(checksum), release_checksum, StringComparison.OrdinalIgnoreCase))
