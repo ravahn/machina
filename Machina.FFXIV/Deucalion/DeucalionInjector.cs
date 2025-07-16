7ab97e64fc144d3637180b82ec4bd8034bc6f7a6
Ravahn <Ravahn@hotmail.com>
2025-07-15 21:29:33 -0400

    v2.4.3.1 - Updates to support Korean FFXIV patch 7.20

--- Machina.FFXIV/Deucalion/DeucalionInjector.cs
+++ Machina.FFXIV/Deucalion/DeucalionInjector.cs
@@ -138,7 +138,7 @@ namespace Machina.FFXIV.Deucalion
         {
             get
             {
-                return GameRegion == GameRegion.Korean ? "deucalion-1.1.0.dll" : "deucalion-1.2.1.dll";
+                return GameRegion == GameRegion.Korean ? "deucalion-1.2.1.dll" : "deucalion-1.2.1.dll";
             }
         }
 
@@ -150,7 +150,7 @@ namespace Machina.FFXIV.Deucalion
             get
             {
                 return GameRegion == GameRegion.Korean ?
-                    new Version(1, 1, 0) :
+                    new Version(1, 2, 0) :
                     new Version(1, 2, 0); // supports 1.2.0 + 1.2.1
             }
         }
@@ -166,7 +166,8 @@ namespace Machina.FFXIV.Deucalion
             get
             {
                 return GameRegion == GameRegion.Korean ?
-                    "19-94-1f-2b-ff-b9-d5-1f-92-cd-60-10-5d-25-cd-19-0c-65-78-0f-6c-a0-70-8b-d8-48-3a-4a-fd-df-ea-93" : // 1.1.0
+                    //"19-94-1f-2b-ff-b9-d5-1f-92-cd-60-10-5d-25-cd-19-0c-65-78-0f-6c-a0-70-8b-d8-48-3a-4a-fd-df-ea-93" : // 1.1.0
+                    "ea-bf-ff-9e-6c-f5-84-2f-97-78-1b-ae-66-9d-5b-2b-40-56-a5-88-1b-ed-e2-9c-94-24-b4-62-6c-8f-56-6e" : // 1.2.1
                     "ea-bf-ff-9e-6c-f5-84-2f-97-78-1b-ae-66-9d-5b-2b-40-56-a5-88-1b-ed-e2-9c-94-24-b4-62-6c-8f-56-6e"; // 1.2.1
             }
         }
